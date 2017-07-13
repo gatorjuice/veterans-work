78593f4cb8683f415dd70f51f579d889c3d543d9
James Gates <gatorjuice@gmail.com>
2017-07-12 20:24:15 -0500

    setup react

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+let path = require('path');
+
+module.exports = {
+  entry: './app-frontend/assets/javascripts/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          cacheDirectory: true,
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+}
